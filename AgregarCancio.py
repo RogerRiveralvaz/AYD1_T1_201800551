1b279bac21fa808a12507dab57ae5b4b83ddb9df
RogerRiveralvaz <70917360+RogerRiveralvaz@users.noreply.github.com>
2025-02-17 23:37:59 -0600

    se agrego un Post para agregar canciones

---
+++ AgregarCancio.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+canciones = []
+
+@app.route('/cancion', methods=['POST'])
+def agregar_cancion():
+    data = request.get_json()
+    nombre = data.get('nombre')
+    artista = data.get('artista')
+    album = data.get('album')
+    
+    if not nombre or not artista or not album:
+        return jsonify({"error": "Faltan datos"}), 400
+    
+    cancion = {"nombre": nombre, "artista": artista, "album": album}
+    canciones.append(cancion)
+    
+    return jsonify(cancion), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
